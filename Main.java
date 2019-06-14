d955f5bc761e284ba1048a7346e70b654c07ab38
Choongchoongee <quo1031@gmail.com>
2019-06-15 08:49:23 +0900

    template

--- Main.java
+++ Main.java
@@ -11,5 +11,30 @@ public class Main{
         //read data graph file
         //read the query grap file
         //find the dag for each query graph
+
+    	int numQuery = Integer.parseInt(args[3]);
+    	
+    	readDataGraph(args[1]);
+
+    	for(int i = 0; i < numQuery; i++) {
+    		readQueryGraph(args[2]);
+    		buildDAG();
+    	}
+    	
     }
+    
+    public static void readDataGraph(String a) {
+    	
+    }
+    
+    public static void readQueryGraph(String b) {
+    	
+    }
+    
+    public static void buildDAG() {
+    	
+    }
+    
+    
+    
 }
