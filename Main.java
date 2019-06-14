0bc4590062d0df544e687113a14755deef79eccb
Choongchoongee <quo1031@gmail.com>
2019-06-14 18:32:24 +0900

    initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) {
+        //args[1] : name of data query file
+        //args[2] : name of query graph file
+        //args[3] : the number of query in query graph file
+
+        //read data graph file
+        //read the query grap file
+        //find the dag for each query graph
+    }
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+javac Main.java
+java Main human human_40n 100 > human_40n.dag
